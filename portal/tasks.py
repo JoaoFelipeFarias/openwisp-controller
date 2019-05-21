ff77c2655f537ab725101924608c84e21569d38c
João Felipe <joao.felipe.23@gmail.com>
2019-05-20 21:09:16 -0300

    pooling from coova device seems to work, now to put everything on a model

--- portal/tasks.py
+++ portal/tasks.py
@@ -25,29 +25,30 @@ def coovadevicepool():
         iter_output = iter(output)
         next(iter_output)
         for line in iter_output:
-            if line == '':
-                break
-            else:
-                print(line)
-
-                splitted_line = line.split(' ')
-                print(splitted_line)
-                dict['mac_address'] = splitted_line[0]
-                dict['ip'] = splitted_line[1]
-                dict['status'] = splitted_line[2]
-                dict['session_id'] = splitted_line[3]
-                dict['auth_status'] = splitted_line[4]
-                dict['username'] = splitted_line[5]
-                dict['duration'] = splitted_line[6]
-                dict['idle_time'] = splitted_line[7]
-                dict['input_octets'] = splitted_line[8]
-                dict['max_total_octets'] = splitted_line[9]
-                dict['status_option_swap_octets'] = splitted_line[10]
-                dict['bandwidth_limitation'] = splitted_line[11]
-                dict['original_url'] = splitted_line[14]
-                device_list.append(dict)
-                dict = {}
-                file.write(str(datetime.now()))
+            # if line == '':
+            #     break
+            # else:
+            #     print(line)
+            #
+            #     splitted_line = line.split(' ')
+            #     print(splitted_line)
+            #     dict['mac_address'] = splitted_line[0]
+            #     dict['ip'] = splitted_line[1]
+            #     dict['status'] = splitted_line[2]
+            #     dict['session_id'] = splitted_line[3]
+            #     dict['auth_status'] = splitted_line[4]
+            #     dict['username'] = splitted_line[5]
+            #     dict['duration'] = splitted_line[6]
+            #     dict['idle_time'] = splitted_line[7]
+            #     dict['input_octets'] = splitted_line[8]
+            #     dict['max_total_octets'] = splitted_line[9]
+            #     dict['status_option_swap_octets'] = splitted_line[10]
+            #     dict['bandwidth_limitation'] = splitted_line[11]
+            #     dict['original_url'] = splitted_line[14]
+            #     device_list.append(dict)
+            #     dict = {}
+                #file.write(str(datetime.now()))
+            file.write(line)
 
         # Look for your absolute directory path
         absolute_path = os.path.dirname(os.path.abspath(__file__))
